e62984a18e5e7cb381df5bb694850b458e1ea55d
wonyongML <wonyonglovek@gmail.com>
2021-02-05 16:17:23 +0900

    Create Sign up and change style

--- signup/models.py
+++ signup/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Signup(models.Model):
     user_id = models.CharField('user_id', max_length=20)
     user_pw = models.CharField('user_pw', max_length=20)
+    email = models.CharField('email', max_length=30)
 
     def __str__(self):
         return self.user_id
--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .models import Signup
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
 def index(request):
     return render(request, 'signup/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user_id = request.POST.get('user_id')
+        user_pw = request.Post.get('user_pw')
+        return HttpResponseRedirect('/index/')
+    return render(request, 'index.html')
+
